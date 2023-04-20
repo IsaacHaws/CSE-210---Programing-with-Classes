535b998d8e0bc63155030a01cce9fff75f5e6d9c
Isaac Haws <ighaws2005@gmail.com>
2023-04-20 12:10:54 -0500

    Finished csharp-prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    static void Main(string[] args) {
+        //Ask for the first name
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        //Ask for the last name
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        Console.WriteLine();
+
+        //Print the classic saying
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
     }
 }
