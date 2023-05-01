d21ee309c71cca24cf5c6d67da7d36c44ca7515e
Isaac Haws <ighaws2005@gmail.com>
2023-05-01 11:37:11 -0500

    Completed W03 Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 1996;
+        job1._endYear = 2010;
+
+
+        Job job2 = new Job();
+        job2._jobTitle = "Teacher";
+        job2._company = "Kids Zone";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        
+        Resume myResume = new Resume();
+        myResume._name = "Isaac Haws";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        
+        myResume.Display();
+
     }
 }
