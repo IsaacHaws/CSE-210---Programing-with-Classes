0ff711b4db794ed1ad8db6de5e03a18e5e80f31a
Isaac Haws <ighaws2005@gmail.com>
2023-06-12 10:19:00 -0500

    Added and coded new classes

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,17 @@
+using System;
+
+
+public class Circle : Shape {
+    private double _raduis;
+
+
+    public Circle(string color, double raduis): base(color) {
+        _raduis = raduis;
+    }
+
+
+    public override double GetArea()
+    {
+        return (_raduis * _raduis) * Math.PI;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Red", 5);
+
+        //Console.WriteLine(square.GetColor());
+        //Console.WriteLine(square.GetArea() + "\n");
+
+
+        Rectangle rect = new Rectangle("Blue", 4.5, 8.2);
+
+        //Console.WriteLine(rect.GetColor());
+        //Console.WriteLine(rect.GetArea() + "\n");
+
+
+        Circle circle = new Circle("Green", 10.2);
+
+        //Console.WriteLine(circle.GetColor());
+        //Console.WriteLine(circle.GetArea());
+
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rect);
+        shapes.Add(circle);
+
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The shape is {color} and has an area of {area}\n");
+        }
     }
 }
